bf6f3dec3b9748b90455cb10186f4b462d671138
SilviM77 <81195514+SilviM77@users.noreply.github.com>
2022-04-14 19:16:31 -0400

    Initial commit

---
+++ dictionary-project/src/App.js
@@ -0,0 +1,17 @@
+import logo from "./wcoder.jpg";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo}
+        className="App-logo
+        img-fluid" alt="logo" />
+      </header>
+      Hello
+    </div>
+  );
+}
+
+export default App;
